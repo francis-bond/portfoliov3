d660bf37f31306cc9564649e9ba7f477728f1e2d
Francis Bond <francisbond1996@gmail.com>
2022-03-14 20:07:55 -0500

    added content to correct pages

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <a className="nav-item nav-link" href="./Assets/Resume.pdf" target="_blank"
+        download="FrancisBondResume">Download Resume</a>
+    </div>
+  );
+}
